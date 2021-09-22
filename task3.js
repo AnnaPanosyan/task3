9e575a75ef7443211afd0cef0f889aec323599c0
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-09-22 22:18:33 +0400

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,27 @@
+//task3
+//Write a JavaScript function to get all possible subsets of given length of the given array.
+//Assume that all elements in the array are unique.
+
+function getAllPossibleSubsets(arr, length, result=[]){
+    let arr1=[]
+   debugger
+    if(arr.length===length){
+         result.push(arr)
+         return result
+     }
+     let k=1
+     for(let i=arr.length-1; i>=length-1;i--){
+        for(let j = 0; j <length;j++){
+           if(j<length-1)
+           arr1.push(arr[j])
+      }
+         arr1.push(arr[i])
+         if(arr1.length===length){
+           result.push(arr1)
+            arr1=[]
+          }
+   }
+  arr.shift()
+  getAllPossibleSubsets(arr, length,result)
+   return result
+   }
